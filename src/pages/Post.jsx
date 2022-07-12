f250d995280de7843634a35530d52acad7a9a211
dikshaverma1502 <94672326+dikshaverma1502@users.noreply.github.com>
2022-07-12 15:16:15 +0530

    Add files via upload

---
+++ src/pages/Post.jsx
@@ -0,0 +1,22 @@
+import { posts } from "../data"
+import {useLocation} from "react-router";
+
+const Post = () => {
+
+    const location = useLocation()
+    console.log(location)
+    const path = location.pathname.split("/")[2]
+
+    const post = posts.find((p)=>p.id.toString() === path)
+  return (
+    <div className="post">
+        <img src={post.img} alt="" className="postImg" />
+        <h1 className="postTitle">{post.title}</h1>
+        <p className="postDesc">{post.desc}</p>
+        <p className="postLongDesc">{post.longDesc}</p>
+      post
+    </div>
+  )
+}
+
+export default Post
